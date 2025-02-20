fc20a0d2930b153a189209c410e4a245919904fe
Marco Bizzaro <51394894+0x12b7@users.noreply.github.com>
2025-02-20 04:29:29 +0100

    add extension trait and trait bounds

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,78 @@
+use std::{ops::{Add, Mul, RangeBounds}, process::Output};
+
+pub trait U128Extension<T1, T2>
+where 
+T1: Into<u128>, 
+T2: Into<u8> {
+    fn fixed_point_cast(self, old_decimals: T2, new_decimals: T2) -> T1;
+    fn fixed_point_slice_of(self, percentage: T1, decimals: T2) -> T1;
+    fn fixed_point_percentage_gain(self, new_value: T1, decimals: T2) -> T1;
+    fn fixed_point_percentage_loss(self, new_value: T1, decimals: T2) -> T1;
+    fn fixed_point_add_percentage(self, percentage: T1, decimals: T2) -> T1;
+    fn fixed_point_sub_percentage(self, percentage: T1, decimals: T2) -> T1;
+    fn fixed_point_mul(self, rhs: T1) -> T1;
+    fn fixed_point_div(self, rhs: T1) -> T1;
+    fn checked_fixed_point_cast(self, old_decimals: T2, new_decimals: T2) -> Option<T1>;
+    fn checked_fixed_point_slice_of(self, percentage: T1, decimals: T2) -> Option<T1>;
+    fn checked_fixed_point_percentage_gain(self, new_value: T1, decimals: T2) -> Option<T1>;
+    fn checked_fixed_point_percentage_loss(self, new_value: T1, decimals: T2) -> Option<T1>;
+    fn checked_fixed_point_add_percentage(self, percentage: T1, decimals: T2) -> Option<T1>;
+    fn checked_fixed_point_sub_percentage(self, percentage: T1, decimals: T2) -> Option<T1>;
+    fn checked_fixed_point_mul(self, rhs: T1) -> Option<T1>;
+    fn checked_fixed_point_div(self, rhs: T1) -> Option<T1>;
+}
+
+impl<T1, T2> U128Extension<T1, T2> for T1
+where
+T1: Into<u128>,
+T2: Into<u8> {
+    fn checked_fixed_point_cast(self, old_decimals: T2, new_decimals: T2) -> Option<u128> {
+        let value: u128 = self.into();
+        let old_decimals: u8 = old_decimals.into();
+        let new_decimals: u8 = new_decimals.into();
+        if !old_decimals.representation() {
+            return None;
+        }
+        if !new_decimals.representation() {
+            return None;
+        }
+        if old_decimals == new_decimals {
+            return Some(value);
+        }
+        if value == 0 {
+            return Some(value);
+        }
+        let value = value
+            .checked_mul(new_decimals.representation()?)?
+            .checked_div(old_decimals.representation()?)?;
+        return Some(value);
+    }
+}
+
+
+
+pub trait U8Extension {
+    fn can_handle_fixed_point_math(self) -> bool;
+    fn one_hundred_percent(self) -> Option<u128>;
+    fn representation(self) -> Option<u128>;
+}
+
+impl U8Extension for u8 {
+    fn can_handle_fixed_point_math(self) -> bool {
+        return self >= 2u8;
+    }
+
+    fn one_hundred_percent(self) -> Option<u128> {
+        return 100u128.checked_mul(self.representation()?);
+    }
+
+    fn representation(self) -> Option<u128> {
+        return 10u128.checked_pow(self.into());
+    }
+}
+
+
+
 pub fn slice_of(value: u128, percentage: u128, decimals: u8) -> Option<u128> {
     return mul(div(value, one_hundred_percent_from(decimals)?, decimals)?, percentage, decimals);
 }
@@ -13,18 +88,26 @@ pub fn percentage_loss(old_value: u128, new_value: u128, decimals: u8) -> Option
     if new_value >= old_value {
         return Some(0);
     }
-    return mul(div(sub(old_value, new_value)?, old_value, decimals)?, one_hundred_percent_from(decimals)?, decimals);
+    let one_hundred_percent: u128 = one_hundred_percent_from(decimals)?;
+    let result: u128 = sub(old_value, new_value)?;
+    let result: u128 = div(result, old_value, decimals)?;
+    let result: u128 = mul(result, one_hundred_percent, decimals)?;
+    return Some(result);
 }
 
 pub fn add_percentage(value: u128, percentage: u128, decimals: u8) -> Option<u128> {
-    return add(mul(div(value, one_hundred_percent_from(decimals)?, decimals)?, percentage, decimals)?, value);
+    let scale: u128 = one_hundred_percent_from(decimals)?;
+    let result: u128 = div(value, scale, decimals)?;
+    let result: u128 = mul(result, percentage, decimals)?;
+    let result: u128 = add(result, value)?;
+    return Some(result);
 }
 
 pub fn sub_percentage(value: u128, percentage: u128, decimals: u8) -> Option<u128> {
-    let scale = one_hundred_percent_from(decimals)?;
-    let result = div(value, scale, decimals)?;
-    let result = mul(result, percentage, decimals)?;
-    let result = sub(value, result)?;
+    let scale: u128 = one_hundred_percent_from(decimals)?;
+    let result: u128 = div(value, scale, decimals)?;
+    let result: u128 = mul(result, percentage, decimals)?;
+    let result: u128 = sub(value, result)?;
     return Some(result);
 }
 
@@ -66,18 +149,6 @@ pub fn to_precision(value: u128, old_decimals: u8, new_decimals: u8) -> Option<u
         .checked_div(representation_of(old_decimals)?)?;
     return Some(result);
 }
-
-pub fn one_hundred_percent_from(decimals: u8) -> Option<u128> {
-    return 100u128.checked_mul(representation_of(decimals)?);
-}
-
-pub fn representation_of(decimals: u8) -> Option<u128> {
-    let decimals: u32 = decimals.into();
-    return 10u128.checked_pow(decimals);
-}
-
-pub fn can_handle_fixed_point_math(decimals: u8) -> bool {
-    return decimals >= 2u8;
 }
 
 #[cfg(test)]
@@ -124,6 +195,11 @@ mod tests {
     fn test_add_percentage() -> () {
         let x: u128 = to_precision(5000u128, 2u8, 18u8).unwrap();
         let y: u128 = to_precision(5000u128, 2u8, 18u8).unwrap();
+        
+        5000u128
+            .checked_add_percentage(percentage, decimals)
+            .unwrap();
+
         add_percentage(x, y, 18)
             .map(|value| {
                 let value: u128 = to_precision(value, 18u8, 2u8).unwrap();
@@ -182,20 +258,20 @@ mod tests {
     }
 
     #[test]
-    fn test_representation_of() -> () {
-        representation_of(2u8)
-            .map(|representation| {
-                assert!(representation == 100u128);
+    fn test_u8_representation() -> () {
+        2u8
+            .representation()
+            .map(|value| {
+                assert!(value == 100u128, "{value}");
                 return;
             })
-            .unwrap();
-        return;
+            .expect(msg);
     }
 
     #[test]
-    fn test_can_handle_fixed_point_math() -> () {
-        assert!(can_handle_fixed_point_math(18u8));
-        assert!(!can_handle_fixed_point_math(1u8));
+    fn test_u8_can_handle_fixed_point_math() -> () {
+        assert!(18u8.can_handle_fixed_point_math());
+        assert!(!1u8.can_handle_fixed_point_math());
         return;
     }
 }
