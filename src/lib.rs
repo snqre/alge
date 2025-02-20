0b64d126beae77a609ae209142c5768ba007ba4a
snqre <marco.bizxaro@gmail.com>
2025-02-20 02:43:53 +0100

    update tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub fn slice_of(value: u128, percentage: u128, decimals: u8) -> Option<u128> {
 }
 
 pub fn percentage_gain(old_value: u128, new_value: u128, decimals: u8) -> Option<u128> {
-    if old_value <= new_value {
+    if new_value <= old_value {
         return Some(0);
     }
     return mul(div(sub(new_value, old_value)?, old_value, decimals)?, one_hundred_percent_from(decimals)?, decimals);
@@ -21,7 +21,11 @@ pub fn add_percentage(value: u128, percentage: u128, decimals: u8) -> Option<u12
 }
 
 pub fn sub_percentage(value: u128, percentage: u128, decimals: u8) -> Option<u128> {
-    return sub(mul(div(value, one_hundred_percent_from(decimals)?, decimals)?, percentage, decimals)?, value);
+    let scale = one_hundred_percent_from(decimals)?;
+    let result = div(value, scale, decimals)?;
+    let result = mul(result, percentage, decimals)?;
+    let result = sub(value, result)?;
+    return Some(result);
 }
 
 pub fn add(x: u128, y: u128) -> Option<u128> {
@@ -80,7 +84,80 @@ pub fn can_handle_fixed_point_math(decimals: u8) -> bool {
 mod tests {
     use crate::*;
 
-    
+    #[test]
+    fn test_slice_of() -> () {
+        slice_of(5000000000u128, 2500000000u128, 8u8)
+            .map(|value| {
+                assert!(value == 1250000000u128, "{value}");
+                return;
+            })
+            .unwrap();
+        return;
+    }
+
+    #[test]
+    fn test_percentage_gain() -> () {
+        let old_value: u128 = 5000000000u128;
+        let new_value: u128 = 9000000000u128;
+        percentage_gain(old_value, new_value, 8u8)
+            .map(|value| {
+                assert!(value == 8000000000u128, "{value}");
+                return;
+            })
+            .unwrap();
+        return;
+    }
+
+    #[test]
+    fn test_percentage_loss() -> () {
+        let x: u128 = to_precision(5000, 2, 18).unwrap();
+        let y: u128 = to_precision(2500, 2, 18).unwrap();
+        percentage_loss(x, y, 18)
+            .map(|value| {
+                let value: u128 = to_precision(value, 18, 2).unwrap();
+                assert!(value == 5000, "{value}");
+            })
+            .unwrap();
+    }
+
+    #[test]
+    fn test_add_percentage() -> () {
+        let x: u128 = to_precision(5000u128, 2u8, 18u8).unwrap();
+        let y: u128 = to_precision(5000u128, 2u8, 18u8).unwrap();
+        add_percentage(x, y, 18)
+            .map(|value| {
+                let value: u128 = to_precision(value, 18u8, 2u8).unwrap();
+                assert!(value == 7500u128, "{value}");
+                return;
+            })
+            .unwrap();
+        return;
+    }
+
+    #[test]
+    fn test_mul() -> () {
+        mul(60u128, 3500u128, 2u8)
+            .map(|value| {
+                assert!(value == 2100u128);
+                return;
+            })
+            .unwrap();
+        return;
+    }
+
+    #[test]
+    fn test_div() -> () {
+        let x = to_precision(550u128, 2, 18).unwrap_or(0);
+        let y = to_precision(5000, 2, 18).unwrap_or(0);
+        sub_percentage(x, y, 18u8) // 5.50, 0.15
+            .map(|value| {
+                let z = to_precision(value, 18, 2).unwrap_or(0);
+                assert!(z == 275u128, "{z}"); // 36.66
+                return;
+            })
+            .unwrap();
+        return;
+    }
 
     #[test]
     fn test_to_precision() -> () {
