b037d7d6e12c46c29f121778374fb2e917d2cad9
Marco Bizzaro <51394894+0x12b7@users.noreply.github.com>
2025-02-22 03:56:17 +0100

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,163 +1,169 @@
-pub trait U128Extension<T1, T2>
-where 
-T1: Into<u128> + From<u128>, 
-T2: Into<u8> {
-    fn fixed_point_cast(self, old_decimals: T2, new_decimals: T2) -> Option<T1>;
-    fn fixed_point_slice_of(self, percentage: T1, decimals: T2) -> Option<T1>;
-    fn fixed_point_percentage_gain(self, new_value: T1, decimals: T2) -> Option<T1>;
-    fn fixed_point_percentage_loss(self, new_value: T1, decimals: T2) -> Option<T1>;
-    fn fixed_point_add_percentage(self, percentage: T1, decimals: T2) -> Option<T1>;
-    fn fixed_point_sub_percentage(self, percentage: T1, decimals: T2) -> Option<T1>;
-    fn fixed_point_mul(self, rhs: T1, decimals: T2) -> Option<T1>;
-    fn fixed_point_div(self, rhs: T1, decimals: T2) -> Option<T1>;
+// # Math
+
+pub type MathResult<T1> = Result<T1, MathError>;
+
+#[derive(Debug)]
+pub enum MathError {
+    Overflow,
+    Underflow,
+    DivByZero,
+    UnsupportedDecimals
 }
 
-impl<T1, T2> U128Extension<T1, T2> for T1
-where
-T1: Into<u128> + From<u128>,
-T2: Into<u8> {
-    fn fixed_point_cast(self, old_decimals: T2, new_decimals: T2) -> Option<T1> {
-        let value: u128 = self.into();
-        let old_decimals: u8 = old_decimals.into();
-        let new_decimals: u8 = new_decimals.into();
-        if !old_decimals.can_handle_fixed_point_math() {
-            return None;
-        }
-        if !new_decimals.can_handle_fixed_point_math() {
-            return None;
-        }
-        if old_decimals == new_decimals {
-            return Some(value.into());
-        }
-        if value == 0 {
-            return Some(value.into());
-        }
-        let value = value
-            .checked_mul(new_decimals.representation()?)?
-            .checked_div(old_decimals.representation()?)?;
-        return Some(value.into());
-    }
+// # U32 Extension
 
-    fn fixed_point_slice_of(self, percentage: T1, decimals: T2) -> Option<T1> {
-        let value: u128 = self.into();
-        let percentage: u128 = percentage.into();
-        let decimals: u8 = decimals.into();
-        let one_hundred_percent: u128 = decimals.one_hundred_percent()?;
-        return value
-            .fixed_point_div(one_hundred_percent, decimals)?
-            .fixed_point_mul(percentage, decimals)
-            .map(|value| value.into());
-    }
+pub trait U32Extension {
+    fn f_one_hundred_percent(self) -> MathResult<u128>;
+    fn f_rep(self) -> MathResult<u128>;
+}
 
-    fn fixed_point_percentage_gain(self, new_value: T1, decimals: T2) -> Option<T1> {
-        let old_value: u128 = self.into();
-        let new_value: u128 = new_value.into();
-        let decimals: u8 = decimals.into();
-        let one_hundred_percent: u128 = decimals.one_hundred_percent()?;
+impl U32Extension for u32 {
+    fn f_one_hundred_percent(self) -> MathResult<u128> {
+        100u128
+            .checked_mul(Self::f_rep(self)?)
+            .ok_or(MathError::Overflow)
+    }    
+
+    fn f_rep(self) -> MathResult<u128> {
+        10u128
+            .checked_pow(self)
+            .ok_or(MathError::Overflow)
+    }
+}
+
+
+// # U128 Extension
+
+pub trait U128Extension {
+    fn f_slice(self, percentage: u128, decimals: u32) -> MathResult<u128>;
+    fn f_percentage_gain(self, new_value: u128, decimals: u32) -> MathResult<u128>;
+    fn f_percentage_loss(self, new_value: u128, decimals: u32) -> MathResult<u128>;
+    fn f_add_percentage(self, percentage: u128, decimals: u32) -> MathResult<u128>;
+    fn f_sub_percentage(self, percentage: u128, decimals: u32) -> MathResult<u128>;
+    fn f_cast(self, old_decimals: u32, new_decimals: u32) -> MathResult<u128>;
+    fn f_mul(self, rhs: u128, decimals: u32) -> MathResult<u128>;
+    fn f_div(self, rhs: u128, decimals: u32) -> MathResult<u128>;
+}
+
+impl U128Extension for u128 {
+    fn f_slice(self, percentage: u128, decimals: u32) -> MathResult<u128> {
+        self
+            .f_div(
+                10u128
+                    .checked_pow(decimals)
+                    .ok_or(MathError::Overflow)?
+                    .checked_mul(100u128)
+                    .ok_or(MathError::Overflow)?,
+                decimals
+            )?
+            .f_mul(percentage, decimals)
+    }
+    
+    fn f_percentage_gain(self, new_value: u128, decimals: u32) -> MathResult<u128> {
+        let old_value: u128 = self;
+        let new_value: u128 = new_value;
+        let one_hundred_percent: u128 = decimals.f_one_hundred_percent()?;
         if new_value <= old_value {
-            return Some(0u128.into());
+            return Ok(0u128);
         }
-        return new_value
-            .checked_sub(old_value)?
-            .fixed_point_div(old_value, decimals)?
-            .fixed_point_mul(one_hundred_percent, decimals)
-            .map(|value| value.into());
+        new_value
+            .checked_sub(old_value)
+            .ok_or(MathError::Underflow)?
+            .f_div(old_value, decimals)?
+            .f_mul(one_hundred_percent, decimals)
     }
 
-    fn fixed_point_percentage_loss(self, new_value: T1, decimals: T2) -> Option<T1> {
-        let old_value: u128 = self.into();
-        let new_value: u128 = new_value.into();
-        let decimals: u8 = decimals.into();
-        let one_hundred_percent: u128 = decimals.one_hundred_percent()?;
+    fn f_percentage_loss(self, new_value: u128, decimals: u32) -> MathResult<u128> {
+        let old_value: u128 = self;
+        let new_value: u128 = new_value;
+        let one_hundred_percent: u128 = decimals.f_one_hundred_percent()?;
         if new_value >= old_value {
-            return Some(0u128.into());
+            return Ok(0u128);
         }
-        return old_value
-            .checked_sub(new_value)?
-            .fixed_point_div(old_value, decimals)?
-            .fixed_point_mul(one_hundred_percent, decimals)
-            .map(|value| value.into());
+        old_value
+            .checked_sub(new_value)
+            .ok_or(MathError::Underflow)?
+            .f_div(old_value, decimals)?
+            .f_mul(one_hundred_percent, decimals)
     }
 
-    fn fixed_point_add_percentage(self, percentage: T1, decimals: T2) -> Option<T1> {
-        let value: u128 = self.into();
-        let percentage: u128 = percentage.into();
-        let decimals: u8 = decimals.into();
-        let one_hundred_percent: u128 = decimals.one_hundred_percent()?;
-        return value
-            .fixed_point_div(one_hundred_percent, decimals)?
-            .fixed_point_mul(percentage, decimals)?
-            .checked_add(value)
-            .map(|value| value.into());
+    fn f_add_percentage(self, percentage: u128, decimals: u32) -> MathResult<u128> {
+        let one_hundred_percent: u128 = decimals.f_one_hundred_percent()?;
+        self
+            .f_div(one_hundred_percent, decimals)?
+            .f_mul(percentage, decimals)?
+            .checked_add(self)
+            .ok_or(MathError::Overflow)
     }
 
-    fn fixed_point_sub_percentage(self, percentage: T1, decimals: T2) -> Option<T1> {
-        let value: u128 = self.into();
-        let percentage: u128 = percentage.into();
-        let decimals: u8 = decimals.into();
-        let one_hundred_percent: u128 = decimals.one_hundred_percent()?;
-        let result: u128 = value
-            .fixed_point_div(one_hundred_percent, decimals)?
-            .fixed_point_mul(percentage, decimals)?;
-        return value
-            .checked_sub(result)
-            .map(|value| value.into());
+    fn f_sub_percentage(self, percentage: u128, decimals: u32) -> MathResult<u128> {
+        let one_hundred_percent: u128 = decimals.f_one_hundred_percent()?;
+        let amount_less: u128 = self
+            .f_div(one_hundred_percent, decimals)?
+            .f_mul(percentage, decimals)?;
+        self
+            .checked_sub(amount_less)
+            .ok_or(MathError::Underflow)
     }
 
-    fn fixed_point_mul(self, rhs: T1, decimals: T2) -> Option<T1> {
-        let value0: u128 = self.into();
-        let value1: u128 = rhs.into();
-        let decimals: u8 = decimals.into();
-        return value0
-            .checked_mul(value1)?
-            .checked_div(decimals.representation()?)
-            .map(|value| value.into());
+    fn f_cast(self, old_decimals: u32, new_decimals: u32) -> MathResult<u128> {
+        if old_decimals < 2
+        || new_decimals < 2 {
+            return Err(MathError::UnsupportedDecimals);
+        }
+        if self == 0
+        || old_decimals == new_decimals {
+            return Ok(self);
+        }
+        let old_rep: u128 = old_decimals.f_rep()?;
+        let new_rep: u128 = new_decimals.f_rep()?;
+        self
+            .checked_mul(new_rep)
+            .ok_or(MathError::Overflow)?
+            .checked_div(old_rep)
+            .ok_or(MathError::DivByZero)
     }
 
-    fn fixed_point_div(self, rhs: T1, decimals: T2) -> Option<T1> {
-        let value0: u128 = self.into();
-        let value1: u128 = rhs.into();
-        let decimals: u8 = decimals.into();
-        return value0
-            .checked_mul(decimals.representation()?)?
-            .checked_div(value1)
-            .map(|value| value.into());
+    fn f_mul(self, rhs: u128, decimals: u32) -> MathResult<u128> {
+        let rep: u128 = decimals.f_rep()?;
+        self
+            .checked_mul(rhs)
+            .ok_or(MathError::Overflow)?
+            .checked_div(rep)
+            .ok_or(MathError::DivByZero)
     }
-}
-
-pub trait U8Extension {
-    fn can_handle_fixed_point_math(self) -> bool;
-    fn one_hundred_percent(self) -> Option<u128>;
-    fn representation(self) -> Option<u128>;
-}
-
-impl U8Extension for u8 {
-    fn can_handle_fixed_point_math(self) -> bool {
-        return self >= 2u8;
-    }
-
-    fn one_hundred_percent(self) -> Option<u128> {
-        return 100u128.checked_mul(self.representation()?);
-    }
-
-    fn representation(self) -> Option<u128> {
-        return 10u128.checked_pow(self.into());
+    
+    fn f_div(self, rhs: u128, decimals: u32) -> MathResult<u128> {
+        let rep: u128 = decimals.f_rep()?;
+        self
+            .checked_mul(rep)
+            .ok_or(MathError::Overflow)?
+            .checked_div(rhs)
+            .ok_or(MathError::DivByZero)
     }
 }
 
 #[cfg(test)]
-mod tests {
-    use crate::*;
+mod test {
+    use crate::U128Extension;
 
     #[test]
-    fn test2() -> () {
-        500u32.fixed
+    fn f_mul() {
+        4550u128
+            .f_mul(5000u128, 2u32)
+            .map(move |value| {
+                
+            })
+            .expect("");
     }
 
     #[test]
-    fn test_u8_can_handle_fixed_point_math() -> () {
-        assert!(18u8.can_handle_fixed_point_math());
-        assert!(!1u8.can_handle_fixed_point_math());
-        return;
+    fn f_div() {
+        4550u128
+            .f_div(5000u128, 2u32)
+            .map(move |value| {
+                assert!(value == 91u128, "{value}");
+            })
+            .expect("")
     }
 }
